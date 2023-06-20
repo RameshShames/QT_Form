bba56914a422ac7eb1d4c1839db8af215c613ccf
RameshShames <rameshnewyear@gmail.com>
2023-06-20 09:57:49 +0530

    Backround Color Changed white to balck and tect color changed to white

--- QT_Form_Application/build-Form_Application-Desktop_Qt_6_4_1_MinGW_64_bit-Debug/ui_mainwindow.h
+++ QT_Form_Application/build-Form_Application-Desktop_Qt_6_4_1_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -29,7 +29,7 @@ public:
     QLabel *label_8;
     QPushButton *pushButton;
     QPushButton *pushButton_2;
-    QWidget *widget;
+    QWidget *layoutWidget;
     QVBoxLayout *verticalLayout;
     QLabel *label_2;
     QLabel *label_3;
@@ -37,7 +37,7 @@ public:
     QLabel *label_5;
     QLabel *label_6;
     QLabel *label_7;
-    QWidget *widget1;
+    QWidget *layoutWidget1;
     QVBoxLayout *verticalLayout_2;
     QLineEdit *lineEdit;
     QLineEdit *lineEdit_2;
@@ -57,96 +57,109 @@ public:
         sizePolicy.setVerticalStretch(0);
         sizePolicy.setHeightForWidth(MainWindow->sizePolicy().hasHeightForWidth());
         MainWindow->setSizePolicy(sizePolicy);
+        MainWindow->setMaximumSize(QSize(16777215, 16777215));
+        MainWindow->setStyleSheet(QString::fromUtf8("background-color:black\n"
+""));
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
         label = new QLabel(centralwidget);
         label->setObjectName("label");
         label->setGeometry(QRect(210, 20, 111, 16));
+        label->setStyleSheet(QString::fromUtf8("color:white"));
         label->setAlignment(Qt::AlignCenter);
         label_8 = new QLabel(centralwidget);
         label_8->setObjectName("label_8");
         label_8->setGeometry(QRect(450, 100, 111, 16));
+        label_8->setStyleSheet(QString::fromUtf8("color:white"));
         label_8->setAlignment(Qt::AlignCenter);
         pushButton = new QPushButton(centralwidget);
         pushButton->setObjectName("pushButton");
         pushButton->setGeometry(QRect(160, 400, 181, 51));
+        pushButton->setStyleSheet(QString::fromUtf8("color:white"));
         pushButton_2 = new QPushButton(centralwidget);
         pushButton_2->setObjectName("pushButton_2");
         pushButton_2->setGeometry(QRect(440, 130, 131, 21));
-        widget = new QWidget(centralwidget);
-        widget->setObjectName("widget");
-        widget->setGeometry(QRect(20, 100, 111, 271));
-        verticalLayout = new QVBoxLayout(widget);
+        pushButton_2->setStyleSheet(QString::fromUtf8("color:white"));
+        layoutWidget = new QWidget(centralwidget);
+        layoutWidget->setObjectName("layoutWidget");
+        layoutWidget->setGeometry(QRect(20, 100, 111, 271));
+        verticalLayout = new QVBoxLayout(layoutWidget);
         verticalLayout->setObjectName("verticalLayout");
         verticalLayout->setContentsMargins(0, 0, 0, 0);
-        label_2 = new QLabel(widget);
+        label_2 = new QLabel(layoutWidget);
         label_2->setObjectName("label_2");
+        label_2->setStyleSheet(QString::fromUtf8("color:white"));
         label_2->setAlignment(Qt::AlignCenter);
 
         verticalLayout->addWidget(label_2);
 
-        label_3 = new QLabel(widget);
+        label_3 = new QLabel(layoutWidget);
         label_3->setObjectName("label_3");
+        label_3->setStyleSheet(QString::fromUtf8("color:white"));
         label_3->setAlignment(Qt::AlignCenter);
 
         verticalLayout->addWidget(label_3);
 
-        label_4 = new QLabel(widget);
+        label_4 = new QLabel(layoutWidget);
         label_4->setObjectName("label_4");
+        label_4->setStyleSheet(QString::fromUtf8("color:white"));
         label_4->setAlignment(Qt::AlignCenter);
 
         verticalLayout->addWidget(label_4);
 
-        label_5 = new QLabel(widget);
+        label_5 = new QLabel(layoutWidget);
         label_5->setObjectName("label_5");
+        label_5->setStyleSheet(QString::fromUtf8("color:white"));
         label_5->setAlignment(Qt::AlignCenter);
 
         verticalLayout->addWidget(label_5);
 
-        label_6 = new QLabel(widget);
+        label_6 = new QLabel(layoutWidget);
         label_6->setObjectName("label_6");
+        label_6->setStyleSheet(QString::fromUtf8("color:white"));
         label_6->setAlignment(Qt::AlignCenter);
 
         verticalLayout->addWidget(label_6);
 
-        label_7 = new QLabel(widget);
+        label_7 = new QLabel(layoutWidget);
         label_7->setObjectName("label_7");
+        label_7->setStyleSheet(QString::fromUtf8("color:white"));
         label_7->setAlignment(Qt::AlignCenter);
 
         verticalLayout->addWidget(label_7);
 
-        widget1 = new QWidget(centralwidget);
-        widget1->setObjectName("widget1");
-        widget1->setGeometry(QRect(130, 90, 261, 291));
-        verticalLayout_2 = new QVBoxLayout(widget1);
+        layoutWidget1 = new QWidget(centralwidget);
+        layoutWidget1->setObjectName("layoutWidget1");
+        layoutWidget1->setGeometry(QRect(130, 90, 261, 291));
+        verticalLayout_2 = new QVBoxLayout(layoutWidget1);
         verticalLayout_2->setObjectName("verticalLayout_2");
         verticalLayout_2->setContentsMargins(0, 0, 0, 0);
-        lineEdit = new QLineEdit(widget1);
+        lineEdit = new QLineEdit(layoutWidget1);
         lineEdit->setObjectName("lineEdit");
 
         verticalLayout_2->addWidget(lineEdit);
 
-        lineEdit_2 = new QLineEdit(widget1);
+        lineEdit_2 = new QLineEdit(layoutWidget1);
         lineEdit_2->setObjectName("lineEdit_2");
 
         verticalLayout_2->addWidget(lineEdit_2);
 
-        lineEdit_3 = new QLineEdit(widget1);
+        lineEdit_3 = new QLineEdit(layoutWidget1);
         lineEdit_3->setObjectName("lineEdit_3");
 
         verticalLayout_2->addWidget(lineEdit_3);
 
-        lineEdit_4 = new QLineEdit(widget1);
+        lineEdit_4 = new QLineEdit(layoutWidget1);
         lineEdit_4->setObjectName("lineEdit_4");
 
         verticalLayout_2->addWidget(lineEdit_4);
 
-        lineEdit_5 = new QLineEdit(widget1);
+        lineEdit_5 = new QLineEdit(layoutWidget1);
         lineEdit_5->setObjectName("lineEdit_5");
 
         verticalLayout_2->addWidget(lineEdit_5);
 
-        lineEdit_6 = new QLineEdit(widget1);
+        lineEdit_6 = new QLineEdit(layoutWidget1);
         lineEdit_6->setObjectName("lineEdit_6");
 
         verticalLayout_2->addWidget(lineEdit_6);
@@ -164,7 +177,7 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
-        label->setText(QCoreApplication::translate("MainWindow", "Profile", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "Zero", nullptr));
         label_8->setText(QCoreApplication::translate("MainWindow", "Photo ID", nullptr));
         pushButton->setText(QCoreApplication::translate("MainWindow", "Upload", nullptr));
         pushButton_2->setText(QCoreApplication::translate("MainWindow", "Choose from file", nullptr));
